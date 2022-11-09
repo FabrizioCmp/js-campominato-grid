d7c007aaef0f19127e15796427dfc6371fcc587d
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-09 15:15:13 +0100

    campominato funzionamento base (100 caselle)

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@ const btnGenerate = document.querySelector(".btn_generate");
 
 
 btnGenerate.addEventListener("click", function(){
-
-    generateCampo();
+    generateCampo(100);
 })
 
 
 function generateCampo(numCelle){
     const campoEl = document.querySelector(".campo");
-    
+    const cellePerRiga = Math.sqrt(numCelle);
+
+    campoEl.innerHTML = "";
+
+    for(let i = 0; i < numCelle; i++){
+        const newCell = document.createElement("div");
+
+
+        campoEl.append(newCell);
+
+        newCell.style.width = `calc(100% / ${cellePerRiga})`;
+        newCell.classList.add(`cella`);
+        newCell.innerHTML = i+1;
+
+        newCell.addEventListener("click", function(){
+            this.classList.add(`bkg_click`);
+            console.log(`hai cliccato la casella numero ${i+1}`);
+        })
+    }
 }
