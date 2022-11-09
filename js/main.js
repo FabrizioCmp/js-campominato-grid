521049ae15abfdf79ed30c2e6e28f2743dcb46ca
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-09 14:50:37 +0100

    html layout base

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const btnGenerate = document.querySelector(".btn_generate");
+
+
+btnGenerate.addEventListener("click", function(){
+
+    generateCampo();
+})
+
+
+function generateCampo(numCelle){
+    const campoEl = document.querySelector(".campo");
+    
+}
